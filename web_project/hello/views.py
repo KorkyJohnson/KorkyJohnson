9104fa41aa0f71433034dc79c360cd581b10ae1f
KorkyJ <88301804+KorkyJ@users.noreply.github.com>
2021-09-12 23:27:37 -0400

    updated home page using common layout template

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -3,8 +3,17 @@ from django.shortcuts       import render
 from django.http            import HttpResponse
 from django.utils.timezone  import datetime
 
+# def home(request):
+#     return HttpResponse("Hello, Django")
+
 def home(request):
-    return HttpResponse("Hello, Django")
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
 
 def hello_there(request, name):
     #
